c336c22bf5a12a04ba2c2bd31554d0832cb4e548
idk-v1 <104273594+idk-v1@users.noreply.github.com>
2023-10-24 23:36:32 -0500

    Update DLL.h

--- ExampleDLL/DLL.h
+++ ExampleDLL/DLL.h
@@ -1,13 +1,13 @@
 #pragma once
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#include <cstdlib>
 
-extern int main();
+extern int main(int argc, char *argv[]);
 
 bool __stdcall DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
     if (ul_reason_for_call == DLL_PROCESS_ATTACH)
-        main();
-
-    return true;
+        return main(__argc, __argv);
+    return 1;
 }
